5d4508e84575c48fe7e39c66eff14f1b3371db4f
Derrick Anderson <deanderson@solstice.com>
2018-08-08 10:07:59 -0500

    Adds un-versioned files

---
+++ src/test/java/com/solstice/ecommercedemo/ECommerceDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solstice.ecommercedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ECommerceDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
